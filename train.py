18b26894a92ddb011d08c2c7abd8a61187d90153
jaapers2 <74304246+jaapers2@users.noreply.github.com>
2024-04-05 20:45:40 +0200

    Data 2

---
+++ train.py
@@ -0,0 +1,14 @@
+from dataset import Triplet_Dataset_RPLAN
+from torch_geometric.loader import DataLoader
+
+data_dir = 'external_data'
+
+bs = 4
+dataset = Triplet_Dataset_RPLAN(data_dir)
+dataloader = DataLoader(dataset, batch_size=bs, shuffle=True)
+data_batch = next(iter(dataloader))
+print(data_batch)
+
+class GCN(torch.nn.Module):
+	super().__init__()
+	
