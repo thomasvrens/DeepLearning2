654f2d7f7aef456f40d92ce1da244aff3d06c338
jaapers2 <74304246+jaapers2@users.noreply.github.com>
2024-04-05 20:48:51 +0200

    model py add

--- train.py
+++ train.py
@@ -8,7 +8,3 @@ dataset = Triplet_Dataset_RPLAN(data_dir)
 dataloader = DataLoader(dataset, batch_size=bs, shuffle=True)
 data_batch = next(iter(dataloader))
 print(data_batch)
-
-class GCN(torch.nn.Module):
-	super().__init__()
-	
